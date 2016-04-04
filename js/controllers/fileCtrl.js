9cf113a766300d0eb1b418d3edfd1b579191eb2c
georgelima <georgelima11@hotmail.com>
2016-04-04 13:19:29 -0300

    correcao diretiva duplicada

--- js/controllers/fileCtrl.js
+++ js/controllers/fileCtrl.js
@@ -146,19 +146,4 @@
 			}
 		}
 	});
-  var app = angular.module('ngLoagScript', []);
-
-	app.directive('script', function(){
-		return {
-			restrict: 'E',
-			scope: false,
-			link: function(scope, elem, attr){
-				if (attr.type === 'text/javascript-lazy'){
-					var code = elem.text();
-					var f = new Function(code);
-					f();
-				}
-			}
-		}
-	});
 })(angular);
